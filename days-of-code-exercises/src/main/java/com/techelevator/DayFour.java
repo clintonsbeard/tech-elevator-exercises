65f5b47174d1be241d8bd9fa1a1f209b65bf3a4d
Clinton <clintonjbeard@gmail.com>
2019-02-14 09:36:21 -0500

    Committing to pull Thursday individual exercises

---
+++ days-of-code-exercises/src/main/java/com/techelevator/DayFour.java
@@ -0,0 +1,16 @@
+package com.techelevator;
+
+/* Day 4: Start1
+Create an integer method called Start1 that takes in two integer arrays "a" and "b".
+Start with 2 int arrays, a and b, of any length.
+Return how many of the arrays have 1 as their first element.
+
+start1([1, 2, 3], [1, 3]) → 2
+start1([7, 2, 3], [1]) → 1
+start1([1, 2], ) → 1 */
+
+public class DayFour {
+
+	
+	
+}
